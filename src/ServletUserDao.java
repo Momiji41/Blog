7109cb7bd6e4d2434b4b386d8f09e0300368abfb
Jonas701 <77495120+Jonas701@users.noreply.github.com>
2021-04-17 17:31:34 +0200

    Update ServletUserDao.java

--- src/ServletUserDao.java
+++ src/ServletUserDao.java
@@ -1,6 +1,9 @@
 package bdd;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,29 +16,25 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/ServletUserDao")
 public class ServletUserDao extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ServletUserDao() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		PrintWriter out = response.getWriter();
+		Dao dao = new Dao();
+		// Fetch all users from the database
+		List<User> users = dao.fetchAll();
+		// Loop over them to display their informations
+		for (User u: users) {
+			out.print("ID: " + u.getId());
+			out.print(" | Firstname: " + u.getFirstname());
+			out.print(" | Lastname: " + u.getLastname());
+			out.print(" | username: " + u.getUsername());
+			out.print(" | password: **************");
+			out.println("");
+		}
+		// Close the connection to the database
+		DbConnection.close();
 	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
-
+	
 }
+
